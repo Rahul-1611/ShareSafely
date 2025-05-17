245fa9427ef341cf0366158bbfec98ce82193e28
Rahul-1611 <rahuldeshmukh1611@gmail.com>
2025-05-17 16:51:34 -0400

    Saving

---
+++ Frontend/src/components/Upload.jsx
@@ -0,0 +1,10 @@
+function Upload() {
+    return (
+        <>
+            <div>Formm
+                <input type="file" name="fileInput" id="fileInput" />
+            </div>
+        </>
+    )
+}
+export default Upload
